0117b72e131989ecfab3da2ea8a759d44fe3dca4
rjhayy <reitheljay25@gmail.com>
2020-03-12 17:29:39 +0800

    ups

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('auth:api')->group(function () {
 	Route::post('add_items' ,'ItemsController@store');
 	Route::post('add_photos' ,'ItemsController@add_photos');
 	Route::post('edit_items' ,'ItemsController@edit');
-	Route::get('all_items' , 'ItemController@all_items');
+	Route::get('all_items' , 'ItemsController@all_items');
 	Route::get('delete_item/{id}' ,'ItemsController@destroy');
 	Route::get('view_item/{id}' ,'ItemsController@show');
 	
