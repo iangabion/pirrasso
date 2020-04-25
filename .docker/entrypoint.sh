392b3e7f57b645e1b09c18ad12b7991baae925d3
Claude Kirke <claudekirke@gmail.com>
2020-04-25 17:14:25 +0800

    add entrypoint

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /var/www/app
+
+source ./dotenv
+.env set APP_ENV=${APP_ENV}
+.env set APP_URL=${APP_URL}
+.env set DB_HOST=${DB_HOST}
+.env set DB_PORT=${DB_PORT}
+.env set DB_DATABASE=${DB_DATABASE}
+.env set DB_USERNAME=${DB_USERNAME}
+.env set DB_PASSWORD=${DB_PASSWORD}
+
+exec /usr/bin/supervisord -n
