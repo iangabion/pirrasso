861aa86ea57ccce30547d3fda6fdc8a86c839433
Qonvex <qonvex620@gmail.com>
2020-07-21 14:47:36 +0800

    feat: add icons to return of category

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,6 +18,7 @@ class CategoryResource extends JsonResource
         return [
             'id'=> $this->id,
             'category' => $this->name,
+            'icon' => $this->icon,
             'subcategory' => $this->subcategories,
         ];
     }
