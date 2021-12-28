a5187c5b7d9f6ffd769baddd3384cc524aa1acc5
romel45 <romelpausanos@gmail.com>
2021-12-28 14:04:57 +0800

    plug-ins dashboard

--- resources/js/components/dashboard/show.vue
+++ resources/js/components/dashboard/show.vue
@@ -84,6 +84,50 @@
                     </v-card-actions>
                 </v-card>
             </v-flex>
+            <v-container>
+                <v-col>
+  <v-card
+    class="mx-auto text-center"
+    color="green"
+    dark
+    max-width="600"
+  >
+    <v-card-text>
+      <v-sheet color="rgba(0, 0, 0, .12)">
+        <v-sparkline
+          :value="value"
+          color="rgba(255, 255, 255, .7)"
+          height="100"
+          padding="24"
+          stroke-linecap="round"
+          smooth
+        >
+          <template v-slot:label="item">
+            ${{ item.value }}
+          </template>
+        </v-sparkline>
+      </v-sheet>
+    </v-card-text>
+
+    <v-card-text>
+      <div class="text-h4 font-weight-thin">
+        Sales Last 24h
+      </div>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-actions class="justify-center">
+      <v-btn
+        block
+        text
+      >
+        Go to Report
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+                </v-col>
+            </v-container>
         </v-layout>
     </v-container>
 </template>
@@ -93,6 +137,16 @@ export default {
         clients: [],
         items: [],
         sold:[],
+        is_loaded: false,
+value: [
+        423,
+        446,
+        675,
+        510,
+        590,
+        610,
+        760,
+      ],
     }),
     methods: {
         get_clients(){
