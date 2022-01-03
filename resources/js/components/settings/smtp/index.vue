8f7677c2420e1509b16d39362f477ffa00461323
ian <gabionian7@gmail.com>
2022-01-03 13:32:07 +0800

    validation

--- resources/js/components/settings/smtp/index.vue
+++ resources/js/components/settings/smtp/index.vue
@@ -15,7 +15,7 @@
           append-icon="mdi-magnify" class=" mx-4"
         />
       </div>
-      <v-btn @click="dialog2 = true" >
+      <v-btn @click="dialog = true" >
         <v-icon class="add_smtp">
           mdi-database-plus
         </v-icon>
@@ -83,26 +83,104 @@
             </v-card>
           </v-flex>
         </v-layout>
-        <addDialog
+         <v-dialog
+            v-model="dialog"
+            persistent
+            max-width="600px"
+          >
+           
+            <v-card>
+              <form>
+                <v-text-field
+                  v-model="name"
+                  :error-messages="nameErrors"
+                  :counter="10"
+                  label="Name"
+                  required
+                  @input="$v.name.$touch()"
+                  @blur="$v.name.$touch()"
+                ></v-text-field>
+                <v-text-field
+                  v-model="email"
+                  :error-messages="emailErrors"
+                  label="E-mail"
+                  required
+                  @input="$v.email.$touch()"
+                  @blur="$v.email.$touch()"
+                ></v-text-field>
+                <v-select
+                  v-model="select"
+                  :items="items"
+                  :error-messages="selectErrors"
+                  label="Item"
+                  required
+                  @change="$v.select.$touch()"
+                  @blur="$v.select.$touch()"
+                ></v-select>
+                <v-checkbox
+                  v-model="checkbox"
+                  :error-messages="checkboxErrors"
+                  label="Do you agree?"
+                  required
+                  @change="$v.checkbox.$touch()"
+                  @blur="$v.checkbox.$touch()"
+                ></v-checkbox>
+
+                <v-btn
+                  class="mr-4"
+                  @click="submit"
+                >
+                  submit
+                </v-btn>
+             
+              </form>
+            </v-card>
+          </v-dialog>
+        <!-- <addDialog
             :dialog="dialog2"
             @close="dialog2 = false; selected_item_id=0"
             @search="search"
             :id="selected_item_id"
-        ></addDialog>
+        ></addDialog> -->
       </v-container>
     </div>
   </div>
 </template>
 <script>
+import { validationMixin } from 'vuelidate'
+  import { required, maxLength, email } from 'vuelidate/lib/validators'
 import addDialog from './includes/dialog.vue'
 import { SetDefault, DeleteSmtpData, GetAllSmtp } from "@api/smtp.api";
 export default {
+   mixins: [validationMixin],
+
+    validations: {
+      name: { required, maxLength: maxLength(10) },
+      email: { required, email },
+      select: { required },
+      checkbox: {
+        checked (val) {
+          return val
+        },
+      },
+    },
+    
     components : {
         addDialog
     },
   data() {
     return {
-      dialog2: false,
+      name: '',
+      email: '',
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+       checkbox: false,
+      dialog: false,
       form: {
         search: '',
       },
@@ -134,12 +212,42 @@ export default {
         { text: this.$t('settings.smtp.default'), align: 'start', value: 'status_on', width: '10%'},
         { text: 'Action', align: 'start', value: 'actions', sortable: false,   width: '10%'},
       ]
-    }
+    },
+     checkboxErrors () {
+        const errors = []
+        if (!this.$v.checkbox.$dirty) return errors
+        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+        return errors
+      },
+      selectErrors () {
+        const errors = []
+        if (!this.$v.select.$dirty) return errors
+        !this.$v.select.required && errors.push('Item is required')
+        return errors
+      },
+      nameErrors () {
+        const errors = []
+        if (!this.$v.name.$dirty) return errors
+        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.name.required && errors.push('Name is required.')
+        return errors
+      },
+      emailErrors () {
+        const errors = []
+        if (!this.$v.email.$dirty) return errors
+        !this.$v.email.email && errors.push('Must be valid e-mail')
+        !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+      },
   },
   mounted() {
     this.search();
   },
   methods: {
+
+    submit () {
+        this.$v.$touch()
+      },
     changeStatus(id){
       SetDefault(id).then(response => {
         this.search();
