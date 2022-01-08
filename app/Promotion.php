9ad15aae33d6927cc205c86d30c89aeb93945074
Chandun <chanduncachero@gmail.com>
2022-01-08 14:42:15 +0800

    January 8, 2022 3rd commit

--- app/Promotion.php
+++ app/Promotion.php
@@ -8,4 +8,12 @@ class Promotion extends Model
 {
     //
     protected $fillable = ['promotion_title', 'promotion_link' ];
+    
+    // protected $guarded = [];
+
+    public function getFilenameAttribute($value){
+        return $value != null ?  asset('images/promotion/'. $value) : asset('images/default.png') ;
+    }
+
+
 }
