c5fa54db728ff3021ec47ec77b773d481915ca11
Chandun <chanduncachero@gmail.com>
2022-01-12 14:34:19 +0800

    january 12 2022 3rd push

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -224,7 +224,7 @@ class ItemsController extends Controller
                 $item->client_id = Auth::user()->id;
 
                 $item->save();
-//
+//test 1 down
                 // $many[] = $request->images;
                 // if (is_array($many) || is_object($many)){
                 //     foreach($many as $photos){
@@ -235,27 +235,43 @@ class ItemsController extends Controller
                 //     };
                 //     $item->photos()->save($photo);
                 // };                
+//test 2 down
+                // $many[] = $request->images;
+                // if (is_array($many) || is_object($many)){
+
+                //     foreach($many as $photos){
+                //         $photo = new Photos();
+                //         $photo->items_id = $item->id;
+                //         $photo->imageable_id = 0;
+                //             if($photos){
+                //             $image = $photos;  // your base64 encoded
+                //                 list($type, $image) = explode(';', $image);
+                //                 list(, $image)      = explode(',', $image);
+                //                 $data = base64_decode($image);
+                //                 $imageName = time() . '.jpeg';
+                //                 file_put_contents(public_path() . '/' . 'images/items/' . $imageName, $data);
+                //                 $photo->filename = $imageName ;
+                //             };
+                //     }
+                //     $item->photos()->save($photo);
+
+                // }; 
 //
-                $many[] = $request->images;
-                if (is_array($many) || is_object($many)){
+                $photo = new Photos();
+                $photo->items_id = $item->id;
+                //
+                    if($request->input('images')){
+                    $image = $request->input('images');  // your base64 encoded
+                        list($type, $image) = explode(';', $image);
+                        list(, $image)      = explode(',', $image);
+                        $data = base64_decode($image);
+                        $imageName = time() . '.jpeg';
+                        file_put_contents(public_path() . '/' . 'images/items/' . $imageName, $data);
+                        $photo->filename = $imageName ;
+                    };
+                $photo->imageable_id = 0;
+                $item->photos()->save($photo);
 
-                    foreach($many as $photos){
-                        $photo = new Photos();
-                        $photo->items_id = $item->id;
-                        $photo->imageable_id = 0;
-                            if($photos){
-                            $image = $photos;  // your base64 encoded
-                                list($type, $image) = explode(';', $image);
-                                list(, $image)      = explode(',', $image);
-                                $data = base64_decode($image);
-                                $imageName = time() . '.jpeg';
-                                file_put_contents(public_path() . '/' . 'images/items/' . $imageName, $data);
-                                $photo->filename = $imageName ;
-                            };
-                    }
-                    $item->photos()->save($photo);
-
-                }; 
 //
                 if($item ) {
                                 if($request->input('vehicles')){
