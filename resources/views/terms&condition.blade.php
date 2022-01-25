fb5994e162f45e7312c835bc71b6a31f44d55c57
Chandun <chanduncachero@gmail.com>
2022-01-25 16:41:57 +0800

    January 25 2022 1st push

---
+++ resources/views/terms&condition.blade.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/app.css')  }}">
+        <title>Obrero</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 1% 10% 2%;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 60px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class = title>
+            <div class = flex-center>
+                Bon Plan Terms And Condition
+            </div>
+        </div>
+        <br>
+        <div>
+            {{-- <p>Your privacy is important to us. It is Studio Seizh's policy to respect your privacy and comply with any applicable law and regulation regarding any personal information we may collect about you, including across our website, <a href="https://bonplan-backoffice.studioseizh.com/privacy-policy">https://studioseizh.com/privacy-policy</a>, and other sites we own and operate. </p>
+            <p>This policy is effective as of 28 April 2021 and was last updated on 28 April 2021. </p> --}}
+            <h3>Terms And Conditions</h3>
+            <br>
+            <p>These terms and conditions (“Agreement”) set forth the general terms and conditions of your use of the “Bon Plan” mobile application 
+                (“Mobile Application” or “Service”) and any of its related products and services (collectively, “Services”). This Agreement is legally 
+                binding between you (“User”, “you” or “your”) and this Mobile Application developer (“Operator”, “we”, “us” or “our”). If you are entering 
+                into this agreement on behalf of a business or other legal entity, you represent that you have the authority to bind such entity to this agreement, 
+                in which case the terms “User”, “you” or “your” shall refer to such entity. If you do not have such authority, or if you do not agree with the terms of 
+                this agreement, you must not accept this agreement and may not access and use the Mobile Application and Services. By accessing and using the Mobile 
+                Application and Services, you acknowledge that you have read, understood, and agree to be bound by the terms of this Agreement. You acknowledge that this 
+                Agreement is a contract between you and the Operator, even though it is electronic and is not physically signed by you, and it governs your use of the 
+                Mobile Application and Services.</p>
+            <br>
+            <h4>Accounts And Membership</h4>
+            <br>
+            <p>You must be at least 16 years of age to use the Mobile Application and Services. By using the Mobile Application and Services and by agreeing 
+                to this Agreement you warrant and represent that you are at least 16 years of age.</p><br>
+            <p>If you create an account in the Mobile Application, you are responsible for maintaining the security of your account and you are fully responsible 
+                for all activities that occur under the account and any other actions taken in connection with it. We may monitor and review new accounts before you 
+                may sign in and start using the Services. Providing false contact information of any kind may result in the termination of your account. You must 
+                immediately notify us of any unauthorized uses of your account or any other breaches of security. We will not be liable for any acts or omissions by you, 
+                including any damages of any kind incurred as a result of such acts or omissions. We may suspend, disable, or delete your account (or any part thereof) 
+                if we determine that you have violated any provision of this Agreement or that your conduct or content would tend to damage our reputation and goodwill. 
+                If we delete your account for the foregoing reasons, you may not re-register for our Services. We may block your email address and Internet protocol 
+                address to prevent further registration.</p>
+            <br>
+            <h4>User content</h4>
+            <br>
+            <p>We do not own any data, information or material (collectively, “Content”) that you submit in the Mobile Application in the course of using the Service. 
+                You shall have sole responsibility for the accuracy, quality, integrity, legality, reliability, appropriateness, and intellectual property ownership 
+                or right to use of all submitted Content. We may monitor and review the Content in the Mobile Application submitted or created using our Services by you. 
+                You grant us permission to access, copy, distribute, store, transmit, reformat, display and perform the Content of your user account solely as required 
+                for the purpose of providing the Services to you. Without limiting any of those representations or warranties, we have the right, though not the obligation, 
+                to, in our own sole discretion, refuse or remove any Content that, in our reasonable opinion, violates any of our policies or is in any way harmful or 
+                objectionable. You also grant us the license to use, reproduce, adapt, modify, publish or distribute the Content created by you or stored in your user 
+                account for commercial, marketing or any similar purpose. </p>
+            <br>
+            {{-- <ul>
+            <li>Name</li>
+            <li>Email</li>
+            <li>Phone/mobile number</li>
+            <li>Home/mailing address</li>
+            </ul> --}}
+            <h4>Adult Content</h4>
+            <br>
+            <p>Please be aware that there may be certain adult or mature content available in the Mobile Application. Where there is mature or adult content, individuals 
+                who are less than 18 years of age or are not permitted to access such content under the laws of any applicable jurisdiction may not access such content. If 
+                we learn that anyone under the age of 18 seeks to conduct a transaction through the Services, we will require verified parental consent, in accordance with 
+                the Children’s Online Privacy Protection Act of 1998 (“COPPA”). Certain areas of the Mobile Application and Services may not be available to children under 
+                18 under any circumstances.</p>
+            <br>
+            <h4>Backups</h4>
+            <br>
+            <p>We perform regular backups of the Content and will do our best to ensure completeness and accuracy of these backups. In the event of the hardware failure or 
+                data loss we will restore backups automatically to minimize the impact and downtime. </p>
+            <br>
+            {{-- <ul>
+            <li>Use a mobile device or web browser to access our content</li>
+            <li>Contact us via email, social media, or on any similar technologies</li>
+            <li>When you mention us on social media</li>
+            </ul>
+            <p>We may collect, hold, use, and disclose information for the following purposes, and personal information will not be further processed in a manner that is incompatible with these purposes: </p>
+            <p>We may collect, hold, use, and disclose information for the following purposes, and personal information will not be further processed in a manner that is incompatible with these purposes: </p>
+            <ul>
+            <li>to contact and communicate with you</li>
+            <li>for internal record keeping and administrative purposes</li>
+            </ul>
+            <p>Please be aware that we may combine information we collect about you with general information or research data we receive from other trusted sources. </p> --}}
+            <h4>Links to other resources</h4>
+            <br>
+            <p>Although the Mobile Application and Services may link to other resources (such as websites, mobile applications, etc.), we are not, directly or indirectly, 
+                implying any approval, association, sponsorship, endorsement, or affiliation with any linked resource, unless specifically stated herein. Some of the links 
+                in the Mobile Application may be “affiliate links”. This means if you click on the link and purchase an item, the Operator will receive an affiliate 
+                commission. We are not responsible for examining or evaluating, and we do not warrant the offerings of, any businesses or individuals or the content of their 
+                resources. We do not assume any responsibility or liability for the actions, products, services, and content of any other third parties. You should carefully 
+                review the legal statements and other conditions of use of any resource which you access through a link in the Mobile Application. Your linking to any other 
+                off-site resources is at your own risk.</p>
+            <br>
+            <h4>Prohibited Uses</h4>
+            <br>
+            <p>In addition to other terms as set forth in the Agreement, you are prohibited from using the Mobile Application and Services or Content: (a) for any unlawful 
+                purpose; (b) to solicit others to perform or participate in any unlawful acts; (c) to violate any international, federal, provincial or state regulations, 
+                rules, laws, or local ordinances; (d) to infringe upon or violate our intellectual property rights or the intellectual property rights of others; (e) to 
+                arass, abuse, insult, harm, defame, slander, disparage, intimidate, or discriminate based on gender, sexual orientation, religion, ethnicity, race, age, 
+                national origin, or disability; (f) to submit false or misleading information; (g) to upload or transmit viruses or any other type of malicious code that 
+                will or may be used in any way that will affect the functionality or operation of the Mobile Application and Services, third party products and services, 
+                or the Internet; (h) to spam, phish, pharm, pretext, spider, crawl, or scrape; (i) for any obscene or immoral purpose; or (j) to interfere with or circumvent
+                 the security features of the Mobile Application and Services, third party products and services, or the Internet. We reserve the right to terminate your use 
+                 of the Mobile Application and Services for violating any of the prohibited uses.</p>
+            <br>
+            <h3>Limitation of liability</h3>
+            <br>
+            <p>To the fullest extent permitted by applicable law, in no event will the Operator, its affiliates, directors, officers, employees, agents, suppliers or 
+                licensors be liable to any person for any indirect, incidental, special, punitive, cover or consequential damages (including, without limitation, damages 
+                for lost profits, revenue, sales, goodwill, use of content, impact on business, business interruption, loss of anticipated savings, loss of business 
+                opportunity) however caused, under any theory of liability, including, without limitation, contract, tort, warranty, breach of statutory duty, negligence 
+                or otherwise, even if the liable party has been advised as to the possibility of such damages or could have foreseen such damages. To the maximum extent 
+                permitted by applicable law, the aggregate liability of the Operator and its affiliates, officers, employees, agents, suppliers and licensors relating to 
+                the services will be limited to an amount no greater than one dollar or any amounts actually paid in cash by you to the Operator for the prior one month 
+                period prior to the first event or occurrence giving rise to such liability. The limitations and exclusions also apply if this remedy does not fully 
+                compensate you for any losses or fails of its essential purpose.</p>
+            <br>
+            <h3>Indemnification</h3>
+            <br>
+            <p>You agree to indemnify and hold the Operator and its affiliates, directors, officers, employees, agents, suppliers and licensors harmless from and against 
+                any liabilities, losses, damages or costs, including reasonable attorneys’ fees, incurred in connection with or arising from any third party allegations, 
+                claims, actions, disputes, or demands asserted against any of them as a result of or relating to your Content, your use of the Mobile Application and 
+                Services or any willful misconduct on your part.</p>
+            <br>
+            <h3>Dispute resolution</h3>
+            <br>
+            <p>The formation, interpretation, and performance of this Agreement and any disputes arising out of it shall be governed by the substantive and procedural 
+                laws of France without regard to its rules on conflicts or choice of law and, to the extent applicable, the laws of France. The exclusive jurisdiction and 
+                venue for actions related to the subject matter hereof shall be the courts located in France, and you hereby submit to the personal jurisdiction of such 
+                courts. You hereby waive any right to a jury trial in any proceeding arising out of or related to this Agreement. The United Nations Convention on Contracts 
+                for the International Sale of Goods does not apply to this Agreement.</p>
+            <br>
+            <h3>Changes and amendments</h3>
+            <br>
+            <p>We reserve the right to modify this Agreement or its terms related to the Mobile Application and Services at any time at our discretion. When we do, we will 
+                post a notification in the Mobile Application. We may also provide notice to you in other ways at our discretion, such as through the contact information 
+                you have provided.</p>
+            <p>An updated version of this Agreement will be effective immediately upon the posting of the revised Agreement unless otherwise specified. Your continued use 
+                of the Mobile Application and Services after the effective date of the revised Agreement (or such other act specified at that time) will constitute your 
+                consent to those changes.</p>
+            <br>
+            <h3>Acceptance of these terms</h3>
+            <br>
+            <p>You acknowledge that you have read this Agreement and agree to all its terms and conditions. By accessing and using the Mobile Application and Services you 
+                agree to be bound by this Agreement. If you do not agree to abide by the terms of this Agreement, you are not authorized to access or use the Mobile 
+                Application and Services. This terms and conditions policy was created with the help of the terms and conditions generator at
+                 https://www.websitepolicies.com/terms-and-conditions-generator</p>
+            <br>
+            <h3>Contacting Us</h3>
+            <br>
+            <p>If you have any questions, concerns, or complaints regarding this Agreement, we encourage you to contact us using the details below:</p>
+            <br>
+            <p>ohm.conception@gmail.com</p>
+            <p>143 avenue de keradennec 29000 quimper, france</p>
+
+            <br>
+            <br>
+            {{-- <br>
+            <h3>Use of Cookies</h3>
+            <br>
+            <p>We use &ldquo;cookies&rdquo; to collect information about you and your activity across our site. A cookie is a small piece of data that our website stores on your computer, and accesses each time you visit, so we can understand how you use our site. This helps us serve you content based on preferences you have specified. </p>
+            <h3>Limits of Our Policy</h3>
+            <p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and policies of those sites, and cannot accept responsibility or liability for their respective privacy practices. </p>
+            <h3>Changes to This Policy</h3>
+            <p>At our discretion, we may change our privacy policy to reflect updates to our business processes, current acceptable practices, or legislative or regulatory changes. If we decide to change this privacy policy, we will post the changes here at the same link by which you are accessing this privacy policy. </p>
+            <p>If required by law, we will get your permission or give you the opportunity to opt in to or opt out of, as applicable, any new uses of your personal information. </p>
+            <h3>Contact Us</h3>
+            <p>For any questions or concerns regarding your privacy, you may contact us using the following details: </p>
+            <p>Ohm Conception<br />
+        ohm.conception@gmail.com </p> --}}
+        </div>
+    </body>
+</html>
