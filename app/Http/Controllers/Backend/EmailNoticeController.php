93fa7308a082bd77983d779402cd60223c99f23c
ian <gabionian7@gmail.com>
2022-03-15 11:50:33 +0800

    push messages done

--- app/Http/Controllers/Backend/EmailNoticeController.php
+++ app/Http/Controllers/Backend/EmailNoticeController.php
@@ -13,23 +13,21 @@ class EmailNoticeController extends Controller
     public function disapprovementEmail(Request $request)
     {
         $data = array(
-            'category_id'               => $request->category['id'],
-            'category_title'            => $request->category['name'],
-            'client_email'              =>  $request->owner['email'],
-            'client_full_name'         =>  $request->owner['full_name'],
-            'client_mobile'             =>  $request->owner['mobile'],
-            'client_username'           =>  $request->owner['username'],
+            'category id'               =>  $request->category['id'],
+            'category title'            =>  $request->category['name'],
+            'client email'              =>  $request->owner['email'],
+            'client full_name'          =>  $request->owner['full_name'],
+            'client mobile'             =>  $request->owner['mobile'],
+            'client username'           =>  $request->owner['username'],
             'reason'                    =>  $request->reason,
-            'item_title'                =>  $request->title,
-            'item_description'          => $request->description,
-            'item_location'             => $request->location,
-            'item_created'              => $request->created_at,
-            'item_price'                => $request->price,
+            'item title'                =>  $request->title,
+            'item description'          =>  $request->description,
+            'item location'             =>  $request->location,
+            'item created'              =>  $request->created_at,
+            'item price'                =>  $request->price,
             // 'client_email'              =>  $request->owner['email'],
 
         );
-
-        
         Mail::to($request->owner['email'])->send(new DisapprovementNotice($data));
         return back()->with('success', 'Thanks for contacting us!');
     }
@@ -37,23 +35,20 @@ class EmailNoticeController extends Controller
     public function approveEmail(Request $request)
     {
         $data = array(
-            'category_id'               => $request->category['id'],
-            'category_title'            => $request->category['name'],
-            'client_email'              =>  $request->client['email'],
-            'client_account_created'    =>  $request->client['created_at'],
-            'client_full_name'         =>  $request->client['full_name'],
-            'client_mobile'             =>  $request->client['mobile'],
-            'client_username'           =>  $request->client['username'],
-            'item_title'                =>  $request->title,
-            'item_description'          => $request->description,
-            'item_location'             => $request->location,
-            'item_created'              => $request->created_at,
-            'item_price'                => $request->price,
+            'category id'               =>  $request->category['id'],
+            'category title'            =>  $request->category['name'],
+            'client email'              =>  $request->client['email'],
+            'client account created'    =>  $request->client['created_at'],
+            'client full name'          =>  $request->client['full_name'],
+            'client mobile'             =>  $request->client['mobile'],
+            'client username'           =>  $request->client['username'],
+            'item title'                =>  $request->title,
+            'item description'          =>  $request->description,
+            'item location'             =>  $request->location,
+            'item created'              =>  $request->created_at,
+            'item price'                =>  $request->price,
         );
         Mail::to($request->client['email'])->send(new ApproveNotice($data));
         return back()->with('success', 'Thanks for contacting us!');
     }
-
-
-
 }
