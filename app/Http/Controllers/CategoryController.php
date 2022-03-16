7d98c916142c8daab9c8186e7a270c04fe69acba
ian <gabionian7@gmail.com>
2022-03-16 09:54:16 +0800

    favoorites

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -36,7 +36,7 @@ class CategoryController extends Controller
     }
 
     public function get_all(){
-        $items = Items::where('is_approved', 1)->orderBy('created_at', 'desc')->get();
+        $items = Items::where('stock', '>=', 1)->where('is_approved', 1)->orderBy('created_at', 'desc')->get();
         return  ItemResource::collection($items) ;
     }
 
