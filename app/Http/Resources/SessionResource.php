5933b7f9695c1bb4867cbc34c65f84afa4b9c434
Chandun <chanduncachero@gmail.com>
2022-03-22 10:11:23 +0800

    first commit march 22 2022

--- app/Http/Resources/SessionResource.php
+++ app/Http/Resources/SessionResource.php
@@ -24,7 +24,7 @@ class SessionResource extends JsonResource
         $buyer = Client::with('fcm_tokens')->findorfail($this->buyer_id); 
         $seller = Client::with('fcm_tokens')->findorfail($this->seller_id);
         $message = Message::where('session_id', $this->id )->where('is_read', 0)->get();
-        $message2 = Message::where('session_id', $this->id )->where('message_status', '!=', $id)->WhereNull('message_status')->get();
+        $message2 = Message::where('session_id', $this->id )->where('message_status', '!=', $id)->orWhereNull('message_status')->get();
         // $product_review = ProductReview::get();
 
         // $message1 = Message::where('session_id', $this->id )->orderBy('created_at')->get();
